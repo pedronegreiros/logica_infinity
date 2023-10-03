0d0870cd9c2bd34a29c072b1d9df1dda299523da
pedronegreiros <146891764+pedronegreiros@users.noreply.github.com>
2023-10-03 18:58:31 -0300

    Add files via upload

---
+++ aula 1/main.py
@@ -0,0 +1,44 @@
+nome = "Pedro" 
+idade = 17 #tipo string (str)
+idade = 17 #tipo string (str)
+altura = 1.77 #tipo decimal (float)
+matriculado = True #boleano (bool)
+
+print(type(nome))
+print(type(idade))
+print(type(altura))
+print(type(matriculado))
+
+numero1 = 8
+numero2 = 6
+
+print(numero1 - numero2)
+print(numero1 + numero2)
+print(numero1 * numero2)
+
+n1 = 9
+n2 = 4
+print(n1 == n2)
+print(n1 != n2)
+print(n1 > n2)
+print(n1 < n2)
+print(n1 >= n2)
+print(n1 <= n2)
+
+nome_do_aluno = input("DIGITE O SEU NOME:")
+
+print("bem vindo",nome_do_aluno,'!')
+print(f"bem vindo {nome_do_auno}!")
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ aula 1/q2.py
@@ -0,0 +1,31 @@
+# nome = 'Pedro'
+# idade = '17'
+
+# nome = input("digite seu nome:")
+
+# idade = input("digite sua idade:" )
+# print("idade de", nome, ':' ,idade, "nome de",nome, ":", nome)
+
+
+
+
+numero1 = 8
+numero2 = 7
+
+
+
+
+print (numero1+numero2)
+print (numero1-numero2)
+print (numero1*numero2)
+print (numero1/numero2)
+print (numero1%numero2)
+print (numero1//numero2)
+print (numero1**numero2)
+
+
+
+
+
+
+
